034a71e3c66467ee7a4cf66b029879ce562ce12b
Srikanth <sree2chin@gmail.com>
2017-11-29 01:26:00 +0530

    some improvement & still need to fix form

--- routes/command.js
+++ routes/command.js
@@ -8,18 +8,35 @@ var middleware = require("../middleware");
 // ====================
 
 // only these are valid command words.
-var commandWords = ["create", "div", "add", "change"];
+var commandWords = ["create", "add", "make", "build", "div", "change", "modify", "restyle", "color"];
+
+// to handle duplicate keywords.
+var commandWordsMapper = {
+  add: "create",
+  make: "create",
+  build: "create",
+  modify: "change",
+  restyle: "change"
+}
 
 router.get("/getCommand", function(req, res){
   console.log('===============in GET COMMAND=====');
-  console.log(req.query.commandText);
+  console.log(req.query);
   var commandText = req.query.commandText;
+  var counter = null;
+  if(req.query.counter) {
+    counter = req.query.counter;
+  }
   var resultText = "";
   if(commandText && commandText.length) {
   	commandText.toLowerCase().trim();
   	for(var i = 0; i < commandText.split(" ").length; i++) {
   		if(commandWords.indexOf(commandText.split(" ")[i]) != -1) {
-  			resultText += commandText.split(" ")[i];
+        if(commandWordsMapper[commandText.split(" ")[i]]) {
+          resultText += commandWordsMapper[commandText.split(" ")[i]];
+        } else {
+          resultText += commandText.split(" ")[i];
+        }
   		}
   	}
   	console.log("===========resultText");
@@ -29,16 +46,24 @@ router.get("/getCommand", function(req, res){
   			if(err) {
   				console.log(err);
   				res.setHeader('Content-Type', 'application/json');
-      			res.send(JSON.stringify({statusCode: 500}));
+          res.status(400)
+      		res.send({status: 400});
   			} else {
-  				console.log("successful result");
-  				console.log(command[0].script);
+          if(resultText.indexOf('create') != -1) {
+            command[0].script += 'sDiv.setAttribute("id", "s-id-'+counter+'");';
+          }
+          console.log("===========successful", resultText);
+          console.log(command);
   				res.setHeader('Content-Type', 'application/json');
-      			res.send(JSON.stringify(command[0].script));
+          res.status(200);
+      		res.send({data: command[0].script});
   			}
   		})
   	}
   }
+  // res.setHeader('Content-Type', 'application/json');
+  // res.status(200);
+  // res.send({data: "hello"});
 })
 
 module.exports = router;
